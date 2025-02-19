ea44b90f39baa7c20df8413dfd94e4a2f2847181
Jesse Harlan <jesse.harlan@centralia.edu>
2025-02-19 09:06:51 -0800

    Add project files.

---
+++ MultiWindowForm/NewCustomerForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MultiWindowForm
+{
+    partial class NewCustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gbCustomer = new GroupBox();
+            btnSave = new Button();
+            txtPhoneNumber = new TextBox();
+            lblPhoneHeading = new Label();
+            txtEmail = new TextBox();
+            lblEmailHeading = new Label();
+            txtName = new TextBox();
+            lblNameHeading = new Label();
+            gbCustomer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // gbCustomer
+            // 
+            gbCustomer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            gbCustomer.AutoSize = true;
+            gbCustomer.Controls.Add(btnSave);
+            gbCustomer.Controls.Add(txtPhoneNumber);
+            gbCustomer.Controls.Add(lblPhoneHeading);
+            gbCustomer.Controls.Add(txtEmail);
+            gbCustomer.Controls.Add(lblEmailHeading);
+            gbCustomer.Controls.Add(txtName);
+            gbCustomer.Controls.Add(lblNameHeading);
+            gbCustomer.Location = new Point(12, 12);
+            gbCustomer.Name = "gbCustomer";
+            gbCustomer.Size = new Size(419, 161);
+            gbCustomer.TabIndex = 0;
+            gbCustomer.TabStop = false;
+            gbCustomer.Text = "New Customer";
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(6, 116);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtPhoneNumber
+            // 
+            txtPhoneNumber.Location = new Point(103, 82);
+            txtPhoneNumber.Name = "txtPhoneNumber";
+            txtPhoneNumber.Size = new Size(310, 23);
+            txtPhoneNumber.TabIndex = 5;
+            // 
+            // lblPhoneHeading
+            // 
+            lblPhoneHeading.AutoSize = true;
+            lblPhoneHeading.Location = new Point(6, 85);
+            lblPhoneHeading.Name = "lblPhoneHeading";
+            lblPhoneHeading.Size = new Size(91, 15);
+            lblPhoneHeading.TabIndex = 4;
+            lblPhoneHeading.Text = "Phone Number:";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(103, 53);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(310, 23);
+            txtEmail.TabIndex = 3;
+            // 
+            // lblEmailHeading
+            // 
+            lblEmailHeading.AutoSize = true;
+            lblEmailHeading.Location = new Point(6, 56);
+            lblEmailHeading.Name = "lblEmailHeading";
+            lblEmailHeading.Size = new Size(39, 15);
+            lblEmailHeading.TabIndex = 2;
+            lblEmailHeading.Text = "Email:";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(103, 24);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(310, 23);
+            txtName.TabIndex = 1;
+            // 
+            // lblNameHeading
+            // 
+            lblNameHeading.AutoSize = true;
+            lblNameHeading.Location = new Point(6, 27);
+            lblNameHeading.Name = "lblNameHeading";
+            lblNameHeading.Size = new Size(42, 15);
+            lblNameHeading.TabIndex = 0;
+            lblNameHeading.Text = "Name:";
+            // 
+            // NewCustomerForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(440, 184);
+            Controls.Add(gbCustomer);
+            Name = "NewCustomerForm";
+            Text = "New Customer Form";
+            gbCustomer.ResumeLayout(false);
+            gbCustomer.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox gbCustomer;
+        private TextBox txtName;
+        private Label lblNameHeading;
+        private TextBox txtEmail;
+        private Label lblEmailHeading;
+        private Button btnSave;
+        private TextBox txtPhoneNumber;
+        private Label lblPhoneHeading;
+    }
+}
